acfeaa787abba6af764ad9873f7bbcd50badafdd
SteamStashDev <steamstashdev@gmail.com>
2024-11-23 12:58:28 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,5 @@
 let tg = window.Telegram.WebApp;
-
-tg.expand(); //расширяем на все окно  
-
-button.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
-	if (tg.MainButton.isVisible){ //если кнопка показана 
-		tg.MainButton.hide() //скрываем кнопку 
-	}
-  else{ //иначе
-  	tg.MainButton.show() //показываем 
-  }
-});
+        tg.expand();
+        tg.MainButton.setText("Click Me!").show().onClick(function(){
+            tg.showAlert("You clicked the button!");
+        });
