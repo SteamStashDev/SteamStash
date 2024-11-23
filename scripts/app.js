073868fd010da830a01a6d120107e0268ede9c06
SteamStashDev <steamstashdev@gmail.com>
2024-11-23 14:58:27 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,44 @@
 let tg = window.Telegram.WebApp;
-        tg.expand();
-        tg.BackButton.show().onClick(function(){
-            Telegram.WebApp.onEvent('backButtonClicked', callback)
-            tg.showAlert("Back button clicked!");
-        });
+tg.expand();
+
+document.getElementById('showAlert').addEventListener('click', function() {
+    tg.showAlert("This is an alert!");
+});
+
+document.getElementById('showConfirm').addEventListener('click', function() {
+    tg.showConfirm("Are you sure?", function(confirmed) {
+        if (confirmed) {
+            tg.showAlert("You confirmed!");
+        } else {
+            tg.showAlert("You cancelled!");
+        }
+    });
+});
+
+document.getElementById('showPopup').addEventListener('click', function() {
+    tg.showPopup({
+        title: "Popup Title",
+        message: "This is a popup message",
+        buttons: [
+            {id: "ok", type: "ok", text: "OK"},
+            {id: "cancel", type: "cancel", text: "Cancel"}
+        ]
+    }, function(buttonId) {
+        tg.showAlert("You clicked: " + buttonId);
+    });
+});
+
+tg.MainButton.setText("Main Action").show().onClick(function(){
+    tg.showAlert("Main button clicked!");
+});
+
+tg.BackButton.show().onClick(function(){
+    tg.showAlert("Back button clicked!");
+});
+
+tg.onEvent('mainButtonClicked', function(){
+    tg.HapticFeedback.impactOccurred('light');
+});
 
         function sessionStorageSet(key, value) {
             try {
