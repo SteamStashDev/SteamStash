cbcc32a60c33376207d7a64d48f74da0282d1246
SteamStashDev <steamstashdev@gmail.com>
2024-11-23 14:55:08 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,33 @@ let tg = window.Telegram.WebApp;
         tg.BackButton.show().onClick(function(){
             tg.showAlert("Back button clicked!");
         });
+
+        function sessionStorageSet(key, value) {
+            try {
+              window.sessionStorage.setItem('__telegram__' + key, JSON.stringify(value));
+              return true;
+            } catch (e) {}
+            return false;
+          }
+        
+          function sessionStorageGet(key) {
+            try {
+              return JSON.parse(window.sessionStorage.getItem('__telegram__' + key));
+            } catch (e) {}
+            return null;
+          }
+        
+          var appTgVersion = 7.0;
+        
+          var initParams = sessionStorageGet('initParams');
+          if (initParams) {
+            if (!initParams.tgWebAppVersion) {
+              initParams.tgWebAppVersion = appTgVersion;
+            }
+          } else {
+            initParams = {
+              tgWebAppVersion: appTgVersion
+            };
+          }
+        
+          sessionStorageSet('initParams', initParams);
