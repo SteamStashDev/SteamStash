840a64c144f3514c7a07b4f8c840972ef3a33f30
SteamStashDev <steamstashdev@gmail.com>
2024-11-23 13:04:48 +0300

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -3,3 +3,19 @@ let tg = window.Telegram.WebApp;
         tg.MainButton.setText("Click Me!").show().onClick(function(){
             tg.showAlert("You clicked the button!");
         });
+
+// Получаем и декодируем initData
+let initData = tg.initData || '';
+let initDataUnsafe = tg.initDataUnsafe || {};
+
+// Проверяем авторизацию пользователя
+if (initDataUnsafe.user) {
+    console.log("User is authorized:", initDataUnsafe.user.first_name);
+} else {
+    console.log("User is not authorized");
+}
+
+// Отправляем данные обратно боту
+document.getElementById('sendData').addEventListener('click', function() {
+    tg.sendData("Some data from Mini App");
+});
